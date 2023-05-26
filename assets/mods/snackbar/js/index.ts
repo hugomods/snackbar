f88be49ce4e1a7de4ed316fdbbbc5cc478daf050
razonyang <razonyang@gmail.com>
2023-05-26 22:03:36 +0800

    chore: add linter support

--- assets/mods/snackbar/js/index.ts
+++ assets/mods/snackbar/js/index.ts
@@ -1,26 +1,22 @@
 class Snackbar {
-    private container: HTMLElement;
+  private readonly container: HTMLElement
 
-    getContainer() {
-        if (!this.container) {
-            this.container = document.createElement('div')
-            this.container.className = 'snackbars'
-            document.body.appendChild(this.container)
-        }
+  constructor () {
+    this.container = document.createElement('div')
+    this.container.className = 'snackbars'
+    document.body.appendChild(this.container)
+  }
 
-        return this.container
-    }
-
-    add(text: string, duration = 2000) {
-        const msg = document.createElement('div')
-        msg.className = 'snackbar'
-        msg.innerText = text
-        this.getContainer().appendChild(msg)
-        setTimeout(() => {
-            msg.remove()
-        }, duration)
-    }
+  add (text: string, duration = 2000): void {
+    const msg = document.createElement('div')
+    msg.className = 'snackbar'
+    msg.innerText = text
+    this.container.appendChild(msg)
+    setTimeout(() => {
+      msg.remove()
+    }, duration)
+  }
 }
 
-const snackbar = new Snackbar
+const snackbar = new Snackbar()
 export default snackbar
